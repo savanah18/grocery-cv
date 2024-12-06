696f45b2482d78053e45fb090182780d32ffe17a
savanah19 <gpagluba@up.edu.ph>
2024-12-07 06:47:53 +0800

    update

--- web-gui/gradio_web.py
+++ web-gui/gradio_web.py
@@ -21,37 +21,45 @@ with open('classes.json', 'r') as file:
     classes = json.load(file)
 
 
-model_detect = YOLO(paths['detect'], task='detect')  # Replace with your model path if different
-model_segment = YOLO(paths['segment'], task='segment')  # Replace with your model path if different
+model_detect = YOLO(paths['detect'])  # Replace with your model path if different
+model_segment = YOLO(paths['segment'])  # Replace with your model path if different
 
-def video_frame_callback(img, task):
+#print(paths['detect'], paths['segment'])
+
+def video_frame_callback(frame, task):
     start_time = time.time()
-    img_size = (768,1280)
-    img = cv2.resize(img, img_size)
-    img_tensor = torch.from_numpy(img).float()
-    img_tensor = rearrange(img_tensor, "h w c -> 1 c h w")
+    img_size = (1280,768)
+    #print(type(frame),frame.shape,task)
+    #print(frame.shape)
+    img = frame.copy()
+    # rearrange frame with numpy
+    frame = cv2.resize(img, img_size)
+    #print(frame.shape)
+    frame = torch.from_numpy(frame).float()
+    frame = rearrange(frame, "h w c -> 1 c h w")
 
-    model = model_detect if task == "detection" else model_segment
-    _model = paths[task]
+    # print(type(img), img.shape)
+    # img_tensor = rearrange(img_tensor, "h w c -> 1 c h w")
+
+    model = model_detect if task == "detect" else model_segment
     # inference
-    preds = model.predict(img_tensor, device='1')
-    inference_time = time.time() - start_time
-    cv2.putText(img, f"Inference Time: {inference_time:.2f}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0), 2)
+    # print(model)
+    print(img.shape, frame.shape)
+    preds = model.predict(frame, device='cuda:1')
     for result in preds:
         #print(result.names[result.boxes.cls])
         if result.boxes is not None:
             for box in result.boxes:
-                if _model in ["yolo11m", "yolo11n-seg"]:
-                    cls = result.names[str(int(box.cls.item()))]
-                else:
-                    cls = classes[str(int(box.cls.item()))]
+                cls = classes[str(int(box.cls.item()))]
                 conf = box.conf.item()
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(img, f"{cls} {conf:.2f}", (x1, y1 - 15), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0), 2)
+                cv2.putText(img, f"{str(int(box.cls.item()))} {cls} {conf:.2f}", (x1, y1 - 15), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0), 2)
 
+        # add mask xy to image
         if task == "segment" and result.masks is not None:
             for mask in result.masks.xy:
+                # create polygon from a series of mask points
                 points = np.int32([mask])
                 cv2.fillPoly(img, points, (0, 255, 0))
 
@@ -59,7 +67,7 @@ def video_frame_callback(img, task):
     end_time = time.time()
     fps = 1 / (end_time - start_time)
     cv2.putText(img, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0), 2)
-    img = cv2.resize(img, (1280, 720))
+    print(img.shape)
     return img
 
 
@@ -72,7 +80,7 @@ with gr.Blocks() as demo:
             input_img = gr.Image(sources=["webcam"], type="numpy", streaming=True)
             task = gr.Dropdown(choices=["detect", "segment"],
                                     value="detect", label="Task")
-    input_img.stream(video_frame_callback, [input_img, task], [input_img], time_limit=30, stream_every=0.1)
+    input_img.stream(video_frame_callback, [input_img, task], [input_img], time_limit=30, stream_every=0.5)
 
 
 demo.launch(server_name="0.0.0.0", server_port=3123)
